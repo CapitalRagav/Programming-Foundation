33ffdd7f906f80e5d9ab86e7efdc269bf52ba309
Administrator <Administrator@172.31.25.19>
2023-04-26 07:34:27 +0000

    Added some additional course

---
+++ ProgrammingFoundation/src/ArmstrongNumber.java
@@ -0,0 +1,30 @@
+
+public class ArmstrongNumber {
+	
+	public static void main(String[] args) {
+		boolean result = isArmStrong(155);
+		System.out.println(result);
+	}
+	
+	public static boolean isArmStrong(int number) {
+		
+		
+		int digit;
+		int temp = number;
+		int sum = 0;
+		while(number>0) {
+			digit = number % 10;
+			sum = sum + digit*digit*digit;
+			number = number/ 10;
+			
+		}
+		
+		if(temp == sum) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+}
